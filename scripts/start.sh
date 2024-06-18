af0d0b1b137b64409be2b297445e0cfef9c404ba
James Grieve <jgrieve@ualberta.ca>
2024-06-17 20:34:45 -0600

    Change reg token to org;

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@
 REPO=$REPO
 ACCESS_TOKEN=$TOKEN
 
-REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/orgs/Web-Development-UAlberta/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
