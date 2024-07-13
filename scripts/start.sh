4f087b2df876314b07d8567b865ab88b2f604327
James Grieve <jgrieve@ualberta.ca>
2024-07-12 18:01:11 -0600

    Another claude fix;

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,9 +4,11 @@ REPO=$REPO
 ACCESS_TOKEN=$TOKEN
 
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/orgs/Web-Development-UAlberta/actions/runners/registration-token | jq .token --raw-output)
-echo REG TOKEN
+
+echo "REG TOKEN"
 echo ${REG_TOKEN}
-cd /home/docker/actions-runner
+
+cd /app/actions-runner
 
 ./config.sh --url https://github.com/Web-Development-UAlberta --token ${REG_TOKEN}
 
@@ -14,6 +16,7 @@ cleanup() {
     echo "Removing runner..."
     ./config.sh remove --unattended --token ${REG_TOKEN}
 }
+
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
