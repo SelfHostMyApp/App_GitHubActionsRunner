e95e6317d2e015d5885e8a8f426ede1e6a4f8961
James Grieve <jamesonrgrieve@gmail.com>
2025-05-09 16:35:55 -0600

    Working runner;

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM ubuntu:20.04
 ARG DEBIAN_FRONTEND=noninteractive
 RUN apt update -y && apt upgrade -y && apt install curl -y && useradd -m docker && apt install -f -y
-ADD --chown=docker:docker download.sh /home/docker/
+ADD --chown=docker:docker scripts/download.sh /home/docker/
 RUN ./home/docker/download.sh
 RUN ./home/docker/actions-runner/bin/installdependencies.sh
+ADD --chown=docker:docker scripts/start.sh /home/docker/
 RUN chown -R $(id -u docker):$(id -g docker) /home/docker
 USER docker
-RUN /home/docker/actions-runner/config.sh --url https://github.com/austinleblanc/traefik --token ${GH_ACTIONS_TOKEN}
-ENTRYPOINT ["/home/docker/actions-runner/run.sh"]
+ENTRYPOINT ["/home/docker/start.sh"]
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-REPO=$REPO
-ACCESS_TOKEN=$TOKEN
-ORG_NAME=$ORG_NAME
-USER_NAME=$USER_NAME
-
 # Error checking for environment variables
-if [[ -n "$ORG_NAME" && -n "$USER_NAME" ]]; then
-    echo "ERROR: Both ORG_NAME and USER_NAME are set. Please set only one."
+if [[ -n "$GH_ORG_NAME" && -n "$GH_USER_NAME" ]]; then
+    echo "ERROR: Both GH_ORG_NAME and GH_USER_NAME are set. Please set only one."
     exit 1
 fi
 
-if [[ -z "$ORG_NAME" && -z "$USER_NAME" ]]; then
-    echo "ERROR: Neither ORG_NAME nor USER_NAME is set. Please set one."
+if [[ -z "$GH_ORG_NAME" && -z "$GH_USER_NAME" ]]; then
+    echo "ERROR: Neither GH_ORG_NAME nor GH_USER_NAME is set. Please set one."
     exit 1
 fi
 
@@ -25,26 +20,26 @@ if [ -e /var/run/docker.sock ]; then
 fi
 
 # Set up the correct URL and token endpoint based on whether we're using org or user
-if [ -n "$ORG_NAME" ]; then
+if [ -n "$GH_ORG_NAME" ]; then
     # Organization runner setup
-    GITHUB_URL="https://github.com/${ORG_NAME}"
-    TOKEN_URL="https://api.github.com/orgs/${ORG_NAME}/actions/runners/registration-token"
+    GITHUB_URL="https://github.com/${GH_ORG_NAME}"
+    TOKEN_URL="https://api.github.com/orgs/${GH_ORG_NAME}/actions/runners/registration-token"
+    REG_TOKEN=$(curl -X POST -H "Authorization: token ${GH_ACTIONS_TOKEN}" -H "Accept: application/vnd.github+json" ${TOKEN_URL} | jq .token --raw-output)
 else
     # User repository runner setup
-    if [ -z "$REPO" ]; then
-        echo "ERROR: REPO must be set when using USER_NAME"
+    if [ -z "$GH_REPO" ]; then
+        echo "ERROR: GH_REPO must be set when using GH_USER_NAME"
         exit 1
     fi
-    GITHUB_URL="https://github.com/${USER_NAME}/${REPO}"
-    TOKEN_URL="https://api.github.com/repos/${USER_NAME}/${REPO}/actions/runners/registration-token"
+    GITHUB_URL="https://github.com/${GH_USER_NAME}/${GH_REPO}"
+    TOKEN_URL="https://api.github.com/repos/${GH_USER_NAME}/${GH_REPO}/actions/runners/registration-token"
+    REG_TOKEN=$GH_ACTIONS_TOKEN
 fi
-
-REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" ${TOKEN_URL} | jq .token --raw-output)
 
 echo "REG TOKEN"
 echo ${REG_TOKEN}
 
-cd /home/runner
+cd /home/docker/actions-runner
 
 ./config.sh --url ${GITHUB_URL} --token ${REG_TOKEN}
 
